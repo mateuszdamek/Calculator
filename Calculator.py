8baffe6d4151ede0b1c58b660d128be51b561be5
mateuszdamek <mateuszdamek00@gmail.com>
2024-02-16 08:41:00 +0100

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,29 +1,39 @@
-print("[1] - addition\n[2] - subtraction\n[3] - multiplication\n[4] - division\n")
 
-operation = int(input("Select operation: "))
+while (True):
+    try:
+        print("[1] - addition\n[2] - subtraction\n[3] - multiplication\n[4] - division\n")
+        operation = int(input("Select operation: "))
+    except ValueError:
+        print("Only numbers are allowed.\n")
+        continue
 
-match operation:
-    case 1:
-        value1 = int(input("Enter first number: "))
-        value2 = int(input("Enter second number: "))
-        result = value1 + value2
-        print(f"Result: {result}")
-    case 2:
-        value1 = int(input("Enter first number: "))
-        value2 = int(input("Enter second number: "))
-        result = value1 - value2
-        print(f"Result: {result}")
-    case 3:
-        value1 = int(input("Enter first number: "))
-        value2 = int(input("Enter second number: "))
-        result = value1 * value2
-        print(f"Result: {result}")
-    case 4:
-        value1 = int(input("Enter first number: "))
-        value2 = int(input("Enter second number: "))
-        result = value1 / value2
-        print(f"Result: {result}")
-    case other:
-        print("Wrong choice.")
+    match operation:
+        case 1:
+            value1 = int(input("Enter first number: "))
+            value2 = int(input("Enter second number: "))
+            result = value1 + value2
+            print(f"Result: {result}\n")
+
+        case 2:
+            value1 = int(input("Enter first number: "))
+            value2 = int(input("Enter second number: "))
+            result = value1 - value2
+            print(f"Result: {result}\n")
+
+        case 3:
+            value1 = int(input("Enter first number: "))
+            value2 = int(input("Enter second number: "))
+            result = value1 * value2
+            print(f"Result: {result}\n")
+
+        case 4:
+            value1 = int(input("Enter first number: "))
+            value2 = int(input("Enter second number: "))
+            result = value1 / value2
+            print(f"Result: {result}\n")
+
+        case other:
+            print("Wrong choice.\n")
+
 
 
